90a3d2c211cc6e4b2697955a2cc8a77948f84f5f
Smith Scarborough <smithscarborough@gmail.com>
2021-02-17 23:06:47 -0600

    adds basic dummy styling w/ animated burger

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const navToggle = document.querySelector('.nav__toggle');
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+    });
+}  
+
+navSlide();   
